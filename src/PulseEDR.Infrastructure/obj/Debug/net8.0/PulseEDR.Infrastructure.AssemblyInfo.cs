828bde631e9cce4b19ffe0fda031c0ef33efb087
Andreia Zaharia <andreiazaharia224@gmail.com>
2026-05-21 10:10:37 +0300

    chore: convert solution to classic .sln format + pin SDK to 8

--- src/PulseEDR.Infrastructure/obj/Debug/net8.0/PulseEDR.Infrastructure.AssemblyInfo.cs
+++ src/PulseEDR.Infrastructure/obj/Debug/net8.0/PulseEDR.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PulseEDR.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f4842d02513e92de86145ac490904f26b358cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0485f454a4fe15bdff2aff589db5b0bad1971e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PulseEDR.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PulseEDR.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
